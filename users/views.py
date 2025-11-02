41162184b89993d35195137f666ce15e65aafe4a
maria_zhiganova <my.nik.mariann@gmail.com>
2025-11-02 22:55:22 +0300

    add users views (crud)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserRegisterSerializer, UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class UserProfileView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
