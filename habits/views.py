f4865b67209af988e65dab94e23681426db5c307
maria_zhiganova <my.nik.mariann@gmail.com>
2025-11-02 22:55:15 +0300

    add habits views (crud)

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 
-# Create your views here.
+from habits.models import Habit
+from habits.pagination import HabitsPagination
+from habits.permissions import IsOwnerPermission
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(CreateAPIView):
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwnerPermission]
+
+
+class OwnHabitListAPIView(ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class PublicHabitListAPIView(ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwnerPermission]
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwnerPermission]
